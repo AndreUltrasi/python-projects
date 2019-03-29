feb98fa3e6ca7d58f6f0800ac8c728bb4fbeccd0
André Augusto Nunes Vieira <andreultrasi@gmail.com>
2019-03-29 12:01:46 -0300

    Create Anjo.py

---
+++ Anjo.py
@@ -0,0 +1,14 @@
+"""
+tabela INSS
+salario, aliquota
+1751.81, 8%
+2919.72, 9%
+5839.45, 11%
+
+tabela IRFF
+base, aliquota, parcela a decidir
+2826.65, 7.5%, 142.80
+3751.05, 15%, 354.80
+4664.68, 22.5, 636.13
+Acima de 4664.68, 27.5, 869.36
+"""
